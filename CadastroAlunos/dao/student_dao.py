67411771e07d0f627c7c21927c26bf7c6e87612f
RafaSK <rafael012chavess@gmail.com>
2025-06-12 18:11:13 -0300

    student_dao bug

--- CadastroAlunos/dao/student_dao.py
+++ CadastroAlunos/dao/student_dao.py
@@ -4,16 +4,21 @@ class Student_DAO:
     @classmethod
     def save(cls, student: Student):
         with open("data/students.txt","a") as arq:
-            arq.writelines(f"{student.name}|{student.cpf}|{student.adress}|{student.phone_number}|{student.student_id}\n")
+            arq.write(f"{student.name}|{student.cpf}|{student.adress}|{student.phone_number}|{student.student_id}\n")
     
     @classmethod
     def read(cls):
-        with open("data/students.txt","r") as arq:
-            lines = arq.readlines()
-        students_list = []
+        try:
+            with open("data/students.txt","r") as arq:
+                lines = arq.readlines()
 
+        except FileNotFoundError:
+            return "FileNotFound"
+
+        students_list = []
+        
         for line in lines:
             parts = line.strip().split("|")
             students_list.append(Student(parts[0],parts[1],parts[2],parts[3],parts[4]))
-            
+
         return students_list
